f13fbb82d63c48e9b14653e156e996ecb33e6c4e
DhananjaliHerath <nipunikaherath9@gmail.com>
2024-10-27 12:37:55 +0530

    nation hospital first commit

---
+++ src/main/java/com/example/nationaluniversityhospital/NationaluniversityhospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.nationaluniversityhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationaluniversityhospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationaluniversityhospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nationaluniversityhospital/NationaluniversityhospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nationaluniversityhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationaluniversityhospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
